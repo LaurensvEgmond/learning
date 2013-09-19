272bb81fd0b16878221e19bbded844a7142c2256
Laurens van Egmond <info@dium.nl>
2013-09-19 22:12:41 +0200

    Added tests for user first name, last name and profile correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
-
+	#relations
 	has_many :statuses
+
+
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+    #validations
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :profile_name, presence: true, uniqueness: true, format: { with: /a-zA-Z0-9_-/, message: 'Must be formatted correctly.'}
 
     def full_name
     	first_name + " " + last_name
