c527569406a4b9f26576c9ff4c801bb5be1072f9
Laurens van Egmond <info@dium.nl>
2013-09-24 19:10:23 +0200

    Added custom routes for auth and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Treebook::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
+  root 'statuses#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'statuses#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
